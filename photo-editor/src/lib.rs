9de37a4909672d5dd33b62aed173b2b2d0d0131c
Rimon Melamed <melamed.rimon@gmail.com>
2022-03-31 23:04:15 -0400

    added the basic stuff and architecture, so we know what to do for friday

--- photo-editor/src/lib.rs
+++ photo-editor/src/lib.rs
@@ -9,8 +9,10 @@
  */
 
 mod utils;
+mod filters;
 
 use wasm_bindgen::prelude::*;
+use web_sys::console;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -18,7 +20,9 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+// Define our functions which will be visible to the JS code here using the wasm_bindgen attribute
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+pub fn console_log() -> u8 {
+    console::log_1(&"Hello".into());
+    return 10;
 }
--- photo-editor/www/index.js
+++ photo-editor/www/index.js
@@ -1,3 +1,104 @@
-import * as wasm from "hello-wasm-pack";
+/**
+ * This file will contain our bare-bones JS frontend. We will write the HTML directly to the window
+ * and have basic methods for our filters, file uploads, etc. Once these functions are called in this file, they will
+ * reference the rust wasm-bindgen implementations to actually perform the operations on the images. All we really need
+ * here is a basic layout, image display, buttons for filters, and an upload file option.
+ */
 
-wasm.greet();
+import * as wasm from "photo_editor";
+
+/**
+ * First, we write the CSS style for our page
+ */
+document.write(`
+<style>
+ /* Column layout style */
+* {
+    box-sizing: border-box;
+  }
+  
+  .first-column {
+    float: left;
+    width: 20%;
+    padding: 10px;
+    height: 100%;
+  }
+  .second-column {
+    float: left;
+    width: 80%;
+    padding: 10px;
+    height: 100%; 
+  }
+  
+  /* Clear floats after the columns */
+  .row:after {
+    content: "";
+    display: table;
+    clear: both;
+  }
+
+
+</style>
+`);
+
+/** 
+ * Now we just render the HTML of the frontend container for our page
+ */
+document.write(`
+                <html>
+                    <body>
+                        <h2>Welcome to the WASM Photo Editor!</h2>
+                    </body>
+                    <div class="row">
+                        <div class="first-column" style="background-color:#aaa;">
+                            <h2>Image Functions</h2>
+                            <p><button type="button" id="test_btn">Add rust functions!</button></p>
+                            <p>
+                                Upload an image
+                                <input type='file' />
+                            </p>
+                        </div>
+                        <div class="second-column" style="background-color:#bbb;">
+                            <h2>Column 2</h2>
+                            <p>Some text..</p>
+                        </div>
+                    </div>
+                </html>`);
+
+/** 
+ * In this section, we add all of our event listeners for the various buttons on the page (janky, but easiest due to the way our setup is...)
+ */
+document.getElementById("test_btn").addEventListener("click", print_console);
+
+
+/**
+ * In this section, we add our interfacing functions which will be called by the buttons on the frontend, and will subsequently call
+ * the rust wasm code to execute the commands (and perform any other JS actions such as image rendering, etc...)
+ */
+
+/**
+ * Just an example function. Runs a wasm function, and also gets a return value from it, this is basically what we'll be doing
+ * throughout the project I think
+ */
+function print_console() {
+    var a = wasm.console_log();
+    console.log(a);
+}
+
+/**
+ * 
+ */
+window.addEventListener('load', function() {
+    document.querySelector('input[type="file"]').addEventListener('change', function() {
+        if (this.files && this.files[0]) {
+            console.log('here');
+            var img = document.querySelector('img');
+            img.onload = () => {
+                URL.revokeObjectURL(img.src);  // no longer needed, free memory
+            }
+  
+            img.src = URL.createObjectURL(this.files[0]); // set src to blob url
+            console.log(img);
+        }
+    });
+  });
