44749991b6eb9b486d8b8e9ab67302ea922a489d
Rimon Melamed <melamed.rimon@gmail.com>
2022-03-31 21:48:17 -0400

    added some comments and figured out initial architecture

--- photo-editor/src/lib.rs
+++ photo-editor/src/lib.rs
@@ -1,3 +1,13 @@
+/*
+ * This file will contain all of our function interactions with the JS code
+ *
+ * i.e. this means that say, for example, if we have an 'Upload Image' button in index.js,
+ * we will upload the image to a byte stream via JS, then call wasm.load_image or something of the like,
+ * and load_image will be defined here, where we will load the image into rust and manipulate it here,
+ * then send it back to index.js
+ *
+ */
+
 mod utils;
 
 use wasm_bindgen::prelude::*;
@@ -9,11 +19,6 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, photo-editor!");
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}!", name));
 }
